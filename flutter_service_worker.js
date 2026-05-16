1399bbc9e11cf8515130ecfac37c5b80cdff87eb
gh93st <62462574+Muhammad-Radwan@users.noreply.github.com>
2026-05-16 02:16:50 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9920f5dc6c9c59374ffb8f2df230337f
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "3eedb84a37673d1029b90969a1f020cc",
+"flutter_bootstrap.js": "c14a1871075bd2d5bb6b69d61ea4c92a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
